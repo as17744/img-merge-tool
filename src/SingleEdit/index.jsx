ce660b9ad359523498db2bf4bfe3067686dada68
as17744 <as17744@126.com>
2020-08-17 23:10:48 +0800

    边距处理

--- src/SingleEdit/index.jsx
+++ src/SingleEdit/index.jsx
@@ -11,22 +11,52 @@ const mapStateToProps = (state) => {
 class SingleEdit extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            verticalPadding: 80,
+            horizontalPadding: 50,
+        };
+    }
+    changePadding(e, type) {
+        if (type === 'vertical') {
+            this.setState({
+                verticalPadding: e.target.value,
+            });
+        }
+        if (type === 'horizontal') {
+            this.setState({
+                horizontalPadding: e.target.value,
+            });
+        }
     }
     render() {
-        console.log(this.props);
         const {
             img,
         } = this.props;
+        const {
+            verticalPadding,
+            horizontalPadding,
+        } = this.state;
         return (
             <div className={styles.wrapper}>
                 <div className={styles.showArea}>
                     {img && (
-                        <div className={styles.operationPanel}>
+                        <div className={styles.operationPanel} style={{padding: `${horizontalPadding}px ${verticalPadding}px`}}>
                             <img src={img} className={styles.showImg} />
                         </div>
                     )}
                 </div>
-                <div className={styles.editArea}></div>
+                <div className={styles.editArea}>
+                    <div className={styles.setColumn}>
+                        <span>水平边距：</span>
+                        <input type="number" value={verticalPadding} className={styles.setInput} onChange={(e) => { this.changePadding(e, 'vertical') }} />
+                        <span>px</span>
+                    </div>
+                    <div className={styles.setColumn}>
+                        <span>垂直边距：</span>
+                        <input type="number" value={horizontalPadding} className={styles.setInput} onChange={(e) => { this.changePadding(e, 'horizontal') }} />
+                        <span>px</span>
+                    </div>
+                </div>
             </div>
         )
     }
